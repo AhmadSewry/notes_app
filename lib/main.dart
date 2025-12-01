09a5aa15c362e1e4110baa47a3eb01165a4699e4
AhmadSewry <ahmadsewry@gmail.com>
2025-12-01 21:37:06 +0300

    created notes list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/notes_view.dart';
 
+void main() {
+  runApp(const NotesApp());
+}
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
